7de06f19aaeb795ee5ef5c61c3fb9469c26174b8
Amira Shamsudin <ai220042@student.uthm.edu.my>
2024-06-08 03:32:56 +0800

    third commit

--- display_user.php
+++ display_user.php
@@ -1,28 +1,24 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Lab_7";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 $sql = "SELECT matric, name, role FROM users";
 $result = $conn->query($sql);
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>User Information</title>
-    <link rel=stylesheet href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -33,15 +29,23 @@ $result = $conn->query($sql);
         <th>Matric</th>
         <th>Name</th>
         <th>Level</th>
+        <th>Actions</th>
     </tr>
     <?php
     if ($result->num_rows > 0) {
-
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . $row["matric"]. "</td><td>" . $row["name"]. "</td><td>" . $row["role"]. "</td></tr>";
+            echo "<tr>
+                    <td>" . $row["matric"]. "</td>
+                    <td>" . $row["name"]. "</td>
+                    <td>" . $row["role"]. "</td>
+                    <td class='action-buttons'>
+                        <a class='update' href='update_user.php?matric=" . $row["matric"] . "'>Update</a>
+                        <a class='delete' href='delete_user.php?matric=" . $row["matric"] . "' onclick='return confirm(\"Are you sure you want to delete this user?\");'>Delete</a>
+                    </td>
+                  </tr>";
         }
     } else {
-        echo "<tr><td colspan='3'>No results found</td></tr>";
+        echo "<tr><td colspan='4'>No results found</td></tr>";
     }
     $conn->close();
     ?>
